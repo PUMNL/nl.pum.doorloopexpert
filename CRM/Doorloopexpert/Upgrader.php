b2c4b6f9332da804cdefe86f883eb887c7ccc599
Jaap Jansma <jaap@edeveloper.nl>
2016-11-14 10:56:39 +0100

    Fixed PUM issue #3601: renamed custom field for datum afwijzing expert and made that field only viewable.

--- CRM/Doorloopexpert/Upgrader.php
+++ CRM/Doorloopexpert/Upgrader.php
@@ -10,4 +10,18 @@ class CRM_Doorloopexpert_Upgrader extends CRM_Doorloopexpert_Upgrader_Base {
     $this->executeCustomDataFile('xml/doorlooptijden_expertapplication.xml');
   }
 
+  /**
+   * Change the label of custom field Datum afwijzing expert and make this field only viewable.
+   *
+   * @return bool
+   */
+  public function upgrade_1001() {
+    $customGroupId = civicrm_api3('CustomGroup', 'getvalue', array('name' => 'doorlooptijden_expert_application', 'return' => 'id'));
+    $customField = civicrm_api3('CustomField', 'getsingle', array('name' => 'datum_afwijzing', 'custom_group_id' => $customGroupId));
+    $customField['label'] = 'Datum afwijzing expert';
+    $customField['is_view'] = '1';
+    civicrm_api3('CustomField', 'create', $customField);
+    return true;
+  }
+
 }
